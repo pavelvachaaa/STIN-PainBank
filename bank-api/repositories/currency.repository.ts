51d5f548573dca620e7ffd5709962aedd09c7da1
Pavel Vácha <pavel.vacha@tul.cz>
2023-04-19 19:00:20 +0200

    Added: Payment and currency repository

---
+++ bank-api/repositories/currency.repository.ts
@@ -0,0 +1,29 @@
+import { Service } from "typedi";
+import ICurrencyRepository from "../interfaces/CurrencyRepository.interface.js";
+import ICurrency from "../models/ICurrency.js";
+import { DatabaseService } from "../services/database.service.js";
+
+@Service()
+export class CurrencyRepository implements ICurrencyRepository {
+
+    constructor(private db: DatabaseService) {
+    }
+
+    public save() {
+        throw new Error("Method not implemented.");
+    }
+
+    public getLast(): ICurrency[] {
+        return this.db.chain.get("currencies", []).last().value().data;
+    }
+
+    public doesCurrencyExist(currency: string): boolean {
+        return this.db.chain.get("currencies", []).last().value().data.findIndex(curr => curr.name === currency) > -1
+    }
+
+    public getRate(currency: string): number {
+        throw new Error("Method not implemented.");
+    }
+
+}
+export default CurrencyRepository;
---
+++ bank-api/repositories/payment.repository.ts
@@ -0,0 +1,20 @@
+import { Service } from "typedi";
+import IPaymentRepository from "../interfaces/PaymentRepository.interface.js";
+import IPayment from "../models/IPayment.js";
+import { DatabaseService } from "../services/database.service.js";
+
+@Service()
+export class PaymentRepository implements IPaymentRepository {
+    constructor(private db: DatabaseService) {
+    }
+
+    async save(payment: IPayment) {
+        await this.db.push("payments", [payment]);
+    }
+
+    getByEmail(email: string): IPayment[] {
+        return this.db.chain.get("payments", []).filter({ email: email }).value();
+    }
+
+}
+export default PaymentRepository;
