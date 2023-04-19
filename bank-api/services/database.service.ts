37ddf8d9a84d3e2e18e2de0aa07230528aa2b282
Pavel Vácha <pavel.vacha@tul.cz>
2023-04-19 16:24:53 +0200

    Added: User service and repository

---
+++ bank-api/services/database.service.ts
@@ -0,0 +1,16 @@
+
+import { Service, Inject, Container } from "typedi";
+import Database from "../vendor/pavel_vacha/core/database.js";
+
+/// Singleton servisa pro databázi
+@Service()
+export class DatabaseService extends Database {
+    constructor() {
+        super("./data/db.json", false);
+        this.createConnection();
+    }
+
+    async createConnection() {
+        await super.createConnection();
+    }
+}
