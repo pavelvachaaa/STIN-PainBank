a9cf41c7f8a72456dc3c7328b8a65a15ba817a82
Pavel Vácha <pavel.vacha@tul.cz>
2023-04-26 21:44:09 +0200

    Added: Registrace, dashboard

--- bank-client/src/pages/api/auth/[...nextauth].ts
+++ bank-client/src/pages/api/auth/[...nextauth].ts
@@ -2,68 +2,53 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import NextAuth from "next-auth";
 import type { NextAuthOptions } from "next-auth";
 import { User } from "next-auth";
+import { AuthenticateDTO, authenticate, } from "@/services/auth.service";
+import { HttpCode } from "@/types/interfaces";
 
 export const authOptions: NextAuthOptions = {
-    // Configure one or more authentication providers
     providers: [
-        // ...add more providers here
         CredentialsProvider({
-            // The name to display on the sign in form (e.g. "Sign in with...")
             name: "Credentials",
-            // `credentials` is used to generate a form on the sign in page.
-            // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-            // e.g. domain, username, password, 2FA token, etc.
-            // You can pass any HTML attribute to the <input> tag through the object.
-            credentials: {
-                username: {
-                    label: "Username",
-                    type: "text",
-                    placeholder: "jsmith",
-                },
-                password: {
-                    label: "Password",
-                    type: "password",
-                },
-            },
-            async authorize(credentials, req) {
-                const { username, password } = credentials as any;
-                const res = await fetch("http://localhost:4000/api/v1/auth/login", {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify({
-                        username,
-                        password,
-                    }),
-                });
+            credentials: { auth_request_id: {}, code: {} },
+            async authorize(credentials, req): Promise<any> {
+                const res = await authenticate({ auth_request_id: credentials?.auth_request_id ?? "", code: Number(credentials?.code) });
+                
+                if (!res?.data == null) {
+                    return null;
+                }
+                
+                return {
+                    id: res.data?.user.id,
+                    name: res.data?.user.name,
+                    email: res.data?.user.email,
+                    access_token: res.data?.token
+                }
 
-                const user = await res.json();
-
-                console.log({ user });
-
-                if (res.ok && user) {
-                    return user;
-                } else return null;
             },
         }),
     ],
 
     callbacks: {
-        async jwt({ token, user }) {
-            return { ...token, ...user };
-        },
-        async session({ session, token, user }) {
-            // Send properties to the client, like an access_token from a provider.
-            session.user = token;
-
+        async session({ session, token, user }: { session: any, token: any, user: any }) {
+            session.user.id = token.id;
+            session.accessToken! = token.accessToken;
             return session;
         },
+        async jwt({ token, user, account, profile, isNewUser }) {
+            if (user) {
+                token.id = user.id;
+            }
+            if (account) {
+                token.accessToken = account.access_token;
+            }
+            return token;
+        },
     },
-
     pages: {
         signIn: "/login",
     },
+    secret: process.env.NEXTAUTH_SECRET,
+
 };
 
 export default NextAuth(authOptions);
