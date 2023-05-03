c3fb42b9fc0a13a52e6bc138775ad8d23a324e00
Pavel Vácha <pavel.vacha@tul.cz>
2023-05-03 17:08:05 +0200

    Added: Cron fetching

--- bank-client/src/components/payments/payments_overview.component.tsx
+++ bank-client/src/components/payments/payments_overview.component.tsx
@@ -20,10 +20,10 @@ export default async function PaymentsOverview() {
     const data = await getData();
 
     return (
-        <div className="w-full p-4  max-w-screen-md bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+        <div className="w-full p-4  max-w-screen-md row-span-3 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
             <div className="flex items-center justify-between mb-4">
                 <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">Poslední platby <p className="text-sm text-gray-500 font-normal pt-1">Zobrazujeme 5 posledních výsledků</p> </h5>
-                <a href="/dashboard/payments" className="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">
+                <a href="/dashboard/payments" className="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500 ">
                     Celý výpis
                 </a>
             </div>
