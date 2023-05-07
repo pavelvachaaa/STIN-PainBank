4ee91554dc1f0d6e6ce54f0d0fc18903b7555124
Pavel Vácha <pavel.vacha@tul.cz>
2023-05-07 15:57:17 +0200

    Updated: middleware for nextjs

--- bank-client/src/app/login/page.tsx
+++ bank-client/src/app/login/page.tsx
@@ -1,16 +1,9 @@
-import { redirect } from 'next/navigation';
 import LoginForm from "@/components/login/login_form.component";
-import { getServerSession } from "next-auth";
 
 export const metadata = {
     title: "Přihlášení"
 }
 
-export default async function Login() {
-    if (await getServerSession()) {
-        return redirect("/dashboard");
-    } else
-        return (
-            <LoginForm></LoginForm>
-        );
+export default function Login() {
+    return <LoginForm></LoginForm>
 }
--- bank-client/src/app/register/page.tsx
+++ bank-client/src/app/register/page.tsx
@@ -1,14 +1,8 @@
 import RegisterForm from "@/components/register/register_form.component";
-import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
-
 export const metadata = {
     title: "Registrace"
 }
 
 export default async function Register() {
-    if (await getServerSession()) {
-        return redirect("/dashboard");
-    } else
-        return <RegisterForm></RegisterForm>
+    return <RegisterForm></RegisterForm>
 }
