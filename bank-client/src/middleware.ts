dc4818d024986356eb36f18767af612104e99cb1
Pavel Vácha <pavel.vacha@tul.cz>
2023-05-08 03:54:53 +0200

    Fix: doesnt redirect anymore

--- bank-client/src/middleware.ts
+++ bank-client/src/middleware.ts
@@ -15,7 +15,7 @@ export default async function middleware(req: NextRequest) {
         secret: process.env.NEXTAUTH_SECRET,
     });
 
-    if (!session && (path !== "/register") && path !== "/login") {
+    if (!session && (path !== "/register") && path !== "/login" && path !== "/") {
         return NextResponse.redirect(new URL('/login', req.url))
     } else if (session && (path === '/login' || path === '/register')) {
         return NextResponse.redirect(new URL('/dashboard', req.url))
